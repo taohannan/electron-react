c88eb1d9bad74d2f3480dc7b0b9a2c3c0e0fe30c
taohannan <taohannan@yahoo.com>
2018-02-25 14:56:17 +0800

    mockup screen
    Added mockup and transparent

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,79 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, Electron!</h1>
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+        <h1>Big Day Countdown</h1>
+        <div className="App">
+        <div >
+          <h1 className="App-title "> Weekend in Paris</h1>
+        </div>
+
+        <div className="Clock">
+          <span className="Clock-time">30</span>
+          <span className="Clock-desc">days</span>
+        </div>
+        <div className="Clock">
+          <span className="Clock-time">30</span>
+          <span className="Clock-desc">hours</span>
+        </div>
+        <div className="Clock">
+        <span className="Clock-time">30</span>
+        <span className="Clock-desc">time</span>
+      </div>
+      <div className="Clock">
+      <span className="Clock-time">30</span>
+      <span className="Clock-desc">minutes</span>
+    </div>
+
+
+      </div>
+      <div className="App">
+      <div >
+        <h1 className="App-title "> Weekend in KL</h1>
+      </div>
+
+      <div className="Clock">
+        <span className="Clock-time">30</span>
+        <span className="Clock-desc">days</span>
+      </div>
+      <div className="Clock">
+        <span className="Clock-time">30</span>
+        <span className="Clock-desc">hours</span>
+      </div>
+      <div className="Clock">
+      <span className="Clock-time">30</span>
+      <span className="Clock-desc">time</span>
+    </div>
+    <div className="Clock">
+    <span className="Clock-time">30</span>
+    <span className="Clock-desc">minutes</span>
+  </div>
+
+
+    </div>
+    <div className="App">
+    <div >
+      <h1 className="App-title "> Weekend in Bangkok</h1>
+    </div>
+
+    <div className="Clock">
+      <span className="Clock-time">30</span>
+      <span className="Clock-desc">days</span>
+    </div>
+    <div className="Clock">
+      <span className="Clock-time">30</span>
+      <span className="Clock-desc">hours</span>
+    </div>
+    <div className="Clock">
+    <span className="Clock-time">30</span>
+    <span className="Clock-desc">time</span>
+  </div>
+  <div className="Clock">
+  <span className="Clock-time">30</span>
+  <span className="Clock-desc">minutes</span>
+</div>
+
+
+  </div>
       </div>
     );
   }
